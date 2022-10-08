79ef0407c586f8f846693612d0c898e7b13a6b5b
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-10-08 18:37:28 -0300

    view changes

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -3,15 +3,27 @@ package com.bolsadeideas.springboot.web.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bolsadeideas.springboot.web.app.models.Usuario;
 
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
-	@GetMapping({"/index","/", "/home"})
+	@GetMapping({"/index","/","", "/home"})
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola Spring Framework!");
-		
 		return "index";
+	}
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Ramiro");
+		usuario.setApellido("Castro");
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		return "perfil";
 	}
 
 }
