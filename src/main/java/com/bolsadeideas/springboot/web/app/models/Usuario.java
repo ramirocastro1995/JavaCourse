88fa2d53d9ec962599174f4505d46e3cdc5cd905
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-10-08 20:11:52 -0300

    perfil.html changes

--- src/main/java/com/bolsadeideas/springboot/web/app/models/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/models/Usuario.java
@@ -3,6 +3,8 @@ package com.bolsadeideas.springboot.web.app.models;
 public class Usuario {
 	private String nombre;
 	private String apellido;
+	private String email;
+	
 	public String getNombre() {
 		return nombre;
 	}
@@ -14,6 +16,12 @@ public class Usuario {
 	}
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	
 	
